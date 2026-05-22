cf6eeaba6df527fac9fcc2c3eff3c61b8c539726
berricklab <ganzabruceberrick@gmail.com>
2026-05-22 17:46:38 +0200

    Add Firebase configuration file

---
+++ firebase-config.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+apiKey: 'YOUR_API_KEY',
+authDomain: 'YOUR_PROJECT.firebaseapp.com',
+projectId: 'YOUR_PROJECT_ID',
+storageBucket: 'YOUR_PROJECT.appspot.com',
+messagingSenderId: 'SENDER_ID',
+appId: 'APP_ID'
+};
+
+firebase.initializeApp(firebaseConfig);
